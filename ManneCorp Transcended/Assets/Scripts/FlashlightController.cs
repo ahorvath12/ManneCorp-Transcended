afe2ed855c97f830a4f7eabb397b19fd913703ce
ahorvath12 <ahorvath@scu.edu>
2020-08-12 14:05:12 -0700

    added chasing orb and built maze environment

---
+++ ManneCorp Transcended/Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+
+    private Light flashlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            flashlight.enabled = !flashlight.enabled;
+    }
+}
