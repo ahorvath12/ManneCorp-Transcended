cf50e3b94a8fe0aa0d2c3e8160c843b6a51eed06
ahorvath12 <ahorvath@scu.edu>
2020-08-17 15:07:47 -0700

    basement & moving mannequins

---
+++ ManneCorp Transcended/Assets/Scripts/DollController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DollController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject meshGO;
+    public AudioClip[] clips;
+
+    private Animator anim;
+    private NavMeshAgent agent;
+    private AudioSource audioSource;
+    private Vector3 startingPos, curPos, lastPos;
+    private Quaternion startingRot;
+    private Renderer rend;
+    bool canMove = false;
+    int pose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        rend = meshGO.GetComponent<Renderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (canMove)
+        {
+            lastPos = curPos;
+            if(rend.isVisible)
+            {
+                agent.isStopped = true;
+                audioSource.Stop();
+            }
+            else
+            {
+                agent.isStopped = false;
+                agent.SetDestination(player.transform.position);
+                pose = Random.Range(1, 4);
+                anim.SetInteger("poseNum", pose);
+            }
+
+            curPos = gameObject.transform.position;
+            if (curPos != lastPos && !audioSource.isPlaying)
+            {
+                int index = Random.Range(0, clips.Length);
+                audioSource.clip = clips[index];
+                audioSource.Play();
+            }
+        }
+    }
+
+    public void StartMoving()
+    {
+        anim.SetTrigger("startMoving");
+        canMove = true;
+    }
+
+    public void ReturnToOrigin()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Debug.Log("killed player");
+        }
+    }
+}
---
+++ ManneCorp Transcended/Assets/Scripts/SelectedFlash.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedFlash : MonoBehaviour
+{
+    public int redCol = 250, greenCol = 250, blueCol;
+
+
+    GameObject selectedObject;
+    bool lookingAtObject = false;
+    bool flashingIn = true;
+    bool startedFlashing = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lookingAtObject)
+        {
+            selectedObject.GetComponent<Renderer>().material.color = new Color32((byte) redCol, (byte) greenCol, (byte) blueCol, 25);
+        }
+    }
+
+    private void OnTriggerEnter()
+    {
+        selectedObject = GameObject.Find(CastingToObject.selectedObject);
+        lookingAtObject = true;
+        if (!startedFlashing)
+        {
+            startedFlashing = true;
+            StartCoroutine(FlashObject());
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        startedFlashing = false;
+        lookingAtObject = false;
+        StopCoroutine(FlashObject());
+        selectedObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 255);
+    }
+
+    IEnumerator FlashObject()
+    {
+        while (lookingAtObject)
+        {
+            yield return new WaitForSeconds(0.15f);
+            if(flashingIn)
+            {
+                if (redCol <= 127)
+                    flashingIn = false;
+                else
+                {
+                    redCol -= 25;
+                    greenCol -= 25;
+                }
+            }
+            else
+            {
+                if (redCol >= 250)
+                    flashingIn = true;
+                else
+                {
+                    redCol += 25;
+                    greenCol += 25;
+                }
+            }
+        }
+    }
+}
